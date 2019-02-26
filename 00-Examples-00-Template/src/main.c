75bb694f3eaf359f975c626fde18037ef33c9af8
NakedSolidSnake <cristianosstec@gmail.com>
2019-02-25 23:22:04 -0300

    template

--- 00-Examples-00-Template/src/main.c
+++ 00-Examples-00-Template/src/main.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : 00-Template.c
- Author      : 
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
