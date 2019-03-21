4a2f4bb4031fd5e19b543a63f2e9fb23f227d539
NakedSolidSnake <cristianosstec@gmail.com>
2019-03-21 07:45:55 -0300

    template

---
+++ 00-Estrutura_de_Dados_Descomplicada-00-Template/src/ListaSequencial.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "ListaSequencial.h"
+
+struct lista{
+	int qtd;
+	struct aluno dados[MAX];
+};
+
+Lista *cria_lista()
+{
+	Lista *li;
+	li = (Lista *)malloc(sizeof(struct lista));
+	if(li != NULL)
+		li->qtd = 0;
+	return li;
+}
+
+void libera_lista(Lista *li)
+{
+	free(li);
+}
+
+int tamanho_lista(Lista *li)
+{
+	if(li == NULL)
+		return -1;
+
+	return li->qtd;
+}
+
+int lista_cheia(Lista *li)
+{
+	if(li == NULL)
+		return -1;
+
+	return (li->qtd == MAX);
+}
+
+int lista_vazia(Lista *li)
+{
+	if(li == NULL)
+		return -1;
+
+	return (li->qtd == 0);
+}
