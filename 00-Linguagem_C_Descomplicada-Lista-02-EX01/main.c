2cb2b1876ee5bd377a7cca345e2c4a38567b2a3d
Cristiano Silva <cristianosstec@gmail.com>
2019-07-01 20:03:33 -0300

    Lista 2 ex 1

--- 00-Linguagem_C_Descomplicada-Lista-02-EX01/main.c
+++ 00-Linguagem_C_Descomplicada-Lista-02-EX01/main.c
@@ -1,9 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//Faça um programa que receba dois números e mostre qual deles é o maior.
+
 int main(int argc, char *argv[])
 {
   (void)argc;
   (void)argv;
+  int n1 , n2;
+
+  printf("Entre com o valor 1: ");
+  scanf("%d", &n1);
+  printf("Entre com o valor 2: ");
+  scanf("%d", &n2);
+  if(n1 > n2)
+    printf("Maior valor: %d\n", n1);
+  else
+    printf("Maior valor: %d\n", n2);
+
+
   return EXIT_SUCCESS;
 }
