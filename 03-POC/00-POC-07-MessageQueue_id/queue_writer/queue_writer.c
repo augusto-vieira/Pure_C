f3b889ffcd9fcea2cc57ee8a69462214681529dd
Cristiano Silva <cristianosstec@gmail.com>
2019-07-10 11:08:54 -0300

    loop queue send

--- 03-POC/00-POC-07-MessageQueue_id/queue_writer/queue_writer.c
+++ 03-POC/00-POC-07-MessageQueue_id/queue_writer/queue_writer.c
@@ -16,16 +16,18 @@ int main(int argc, char *argv[])
 
     msgid = queue_init("progfile");
     message.id= 1;
-    strcpy(d.b_data, "Hello World");
-    memcpy(message.b, &d, sizeof(data_t));
-    queue_send(msgid, &message);
-    data_init(&d);
+    while(1)
+    {
+      strcpy(d.b_data, "Hello World");
+      memcpy(message.b, &d, sizeof(data_t));
+      queue_send(msgid, &message);
+      data_init(&d);
 
-    d.id = 2;
-    strcpy(d.b_data, "Hallo Welt");
-    memcpy(message.b, &d, sizeof(data_t));
-    queue_send(msgid, &message);
-
+      d.id = 2;
+      strcpy(d.b_data, "Hallo Welt");
+      memcpy(message.b, &d, sizeof(data_t));
+      queue_send(msgid, &message);
+    }
     return 0;
 }
 
