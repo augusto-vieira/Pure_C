48186cc1dce40b5b8e7f9b9580dd996f9eb56e2d
Cristiano Silva <cristianosstec@gmail.com>
2019-09-08 22:06:27 -0300

    log vars

--- 10-projects/rover_rasp/rover_system/lib/include/log.h
+++ 10-projects/rover_rasp/rover_system/lib/include/log.h
@@ -2,5 +2,6 @@
 #define __LOG_H
 
 void log(const char *module, const char *log_message);
+void logarg(const char *module, const char *log_message, ...);
 
 #endif
--- 10-projects/rover_rasp/rover_system/lib/src/log.c
+++ 10-projects/rover_rasp/rover_system/lib/src/log.c
@@ -2,6 +2,7 @@
 #include <syslog.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 void log(const char *module, const char *log_message)
 {
@@ -9,5 +10,16 @@ void log(const char *module, const char *log_message)
   syslog(LOG_INFO, log_message);
   closelog();
   return;
+}
+
+void logarg(const char *module, const char *log_message, ...)
+{
+  char buffer[1024] = {0};
+  va_list args;
+  va_start(args, log_message);
+  vsprintf(buffer, log_message, args);
+  va_end(args);
+
+  log(module, buffer);
 }
 
