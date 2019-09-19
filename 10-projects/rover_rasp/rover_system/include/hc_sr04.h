cd63fe7cf415b306d165fe00b1f1bc9e850f158c
Cristiano silva de Souza <cristianosstec@gmail.com>
2019-09-18 22:25:57 -0300

    Sensor Ultrasonic

--- 10-projects/rover_rasp/rover_system/include/hc_sr04.h
+++ 10-projects/rover_rasp/rover_system/include/hc_sr04.h
@@ -1,6 +1,9 @@
 #ifndef __HC_SR04_H
 #define __HC_SR04_H
 
+#define TRIGGER     5
+#define ECHO        6
+
 int HC_SR04_init(void);
 int HC_SR04_getCM(void);
 
--- 10-projects/rover_rasp/rover_system/src/hc_sr04.c
+++ 10-projects/rover_rasp/rover_system/src/hc_sr04.c
@@ -1,1 +1,38 @@
+#include <stdlib.h>
+#include <wiringPi.h>
 
+int HC_SR04_init(void)
+{
+  int ret = -1;
+  ret = wiringPiSetup();
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  /* TRIGGER pin must be starting LOW */
+  digitalWrite(TRIGGER, LOW);
+  delay(30);
+
+  return EXIT_SUCCESS;
+}
+
+int HC_SR04_getCM(void)
+{
+  /* Send TRIGGER pulse */
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(TRIGGER, LOW);
+
+
+  /* Wait ECHO pin start */
+  while(digitalRead(ECHO) == LOW);
+
+  /* Wait ECHO end */
+  long startTime = micros();
+  while(digitalRead(ECHO) == HIGH);
+  long travelTime = micros() - startTime;
+
+  /* Convert to distance in cm */
+  int distance = travelTime /58;
+
+  return distance;
+}
